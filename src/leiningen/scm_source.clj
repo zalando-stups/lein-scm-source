177a901a58a45b567315355b5656db86f0ee15b9
Tobias Sarnowski <tobias.sarnowski@zalando.de>
2015-05-13 09:04:23 +0200

    use target-path and git: prefix in url

--- src/leiningen/scm_source.clj
+++ src/leiningen/scm_source.clj
@@ -1,7 +1,8 @@
 (ns leiningen.scm-source
   (:require [clojure.java.shell :as sh]
             [clojure.string :as string]
-            [clojure.data.json :as json]))
+            [clojure.data.json :as json]
+            [clojure.java.io :as io]))
 
 (defn exec
   "Execute a shell command and return its output. Throws an exception on error."
@@ -15,8 +16,8 @@
   "Generates the 'scm-source.json' according to the STUPS documentation."
   [project & args]
   (->> {:revision (exec "git" "rev-parse" "HEAD")
-        :url      (exec "git" "config" "--get" "remote.origin.url")
+        :url      (str "git:" (exec "git" "config" "--get" "remote.origin.url"))
         :status   (exec "git" "status" "--porcelain")
         :author   (exec "id" "-un")}
        (json/write-str)
-       (spit "scm-source.json")))
+       (spit (io/file (:target-path project) "scm-source.json"))))
