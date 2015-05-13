85d4d93d604f95e621d38c0663b1f7980297a583
Tobias Sarnowski <tobias.sarnowski@zalando.de>
2015-05-13 09:04:41 +0200

    Version 0.2.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.zalando.stups/lein-scm-source "0.2.0-SNAPSHOT"
+(defproject org.zalando.stups/lein-scm-source "0.2.0"
   :description "Generates source control origin information file."
   :url "https://github.com/zalando-stups/lein-scm-source"
 
